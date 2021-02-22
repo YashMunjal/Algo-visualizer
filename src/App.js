f1789ae5f526c875ed47930b38c3aafe786718cb
yashmunjal <yashmu45@gmail.com>
2021-02-23 01:54:50 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-// import logo from './logo.svg';
-import SortingVisualizer from './component/SortingVisualizer';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SortingVisualizer></SortingVisualizer>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
